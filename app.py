31af96516286023374e888c339201e1bb38a4ced
hoss1shark <64555397+hoss1shark@users.noreply.github.com>
2022-06-05 17:22:00 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,53 @@
+import flask
+from flask import Flask, request, render_template
+from data import Articles
+from wtforms import Form, TextAreaField, BooleanField, StringField, PasswordField, validators
+from flask_mysqldb import MySQL
+app = Flask(__name__)
+Articles = Articles()
+#Configuring Mysql
+
+app.config['MYSQL_HOST'] = 'localhost'
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('/Home.html')
+
+
+@app.route('/aboutus')
+def about():
+    return render_template('/aboutus.html')
+
+
+@app.route('/articles')
+def articles():
+    return render_template('/articles.html', articles=Articles)
+
+
+@app.route('/article/<string:id>')
+def article(id):
+    return render_template('/article.html', id=id)
+
+
+# setting registerion form
+class RegisertionForm(Form):
+    name = StringField('Name', [validators.length(min=1, max=50)])
+    username = StringField('UserName', [validators.length(min=4, max=50)])
+    email = StringField('Email Address', [validators.length(min=6, max=50)])
+    password = PasswordField(' Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisertionForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template('register.html')
+    return render_template('register.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles =[
+        {
+        'id' : 1,
+        'title': 'Heros Tonight',
+        'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi atque aut blanditiis cum dolorum fuga fugit impedit magni minima, modi, mollitia nesciunt praesentium quae quia soluta. Amet animi quae unde?',
+        'auther':'chris chris',
+        'create_date':'06-05-2020'
+        },
+        {
+            'id': 2,
+            'title': 'Heros yesterday',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi atque aut blanditiis cum dolorum fuga fugit impedit magni minima, modi, mollitia nesciunt praesentium quae quia soluta. Amet animi quae unde?',
+            'auther': 'chris breath',
+            'create_date': '06-05-2020'
+        },
+        {
+            'id': 3,
+            'title': 'Heros Tomorrow',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi atque aut blanditiis cum dolorum fuga fugit impedit magni minima, modi, mollitia nesciunt praesentium quae quia soluta. Amet animi quae unde?',
+            'auther': 'chris treath',
+            'create_date': '06-05-2020'
+        }
+    ]
+    return articles
