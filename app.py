655cafe4e5bf37285536427ab428b17cea46b7ee
hoss1shark <64555397+hoss1shark@users.noreply.github.com>
2022-06-06 16:10:29 +0200

    add articles

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import flask
-from flask import Flask, request, render_template,flash,session,redirect,url_for
-from data import Articles
-from wtforms import Form, StringField, TextAreaField,PasswordField, validators
+from flask import Flask, request, render_template, flash, session, redirect, url_for
+# from data import Articles
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from flask_mysqldb import MySQL
 from passlib.hash import sha256_crypt
 from functools import wraps
 
 app = Flask(__name__)
-Articles = Articles()
+# Articles = Articles()
 # Configuring Mysql
 app.config.update(
     TESTING=True,
@@ -35,12 +35,28 @@ def about():
 
 @app.route('/articles')
 def articles():
-    return render_template('/articles.html', articles=Articles)
+    cur = mysql.connection.cursor()
+    result = cur.execute("select * from articles")
+
+    if result > 0:
+        articles = cur.fetchall()
+        cur.close()
+        return render_template('/articles.html', articles=articles)
+    return render_template('/articles.html')
 
 
 @app.route('/article/<string:id>')
 def article(id):
+    cur = mysql.connection.cursor()
+
+    result = cur.execute("select * from articles where id=%s",(id))
+    if result > 0:
+        article = cur.fetchone()
+        cur.close()
+        return render_template('/article.html', article=article)
     return render_template('/article.html', id=id)
+
+
 
 # setting addarticle form
 class AddArticleForm(Form):
@@ -60,7 +76,7 @@ class RegisertionForm(Form):
     confirm = PasswordField('Repeat Password')
 
 
-@app.route('/register',methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisertionForm(request.form)
     if request.method == 'POST' and form.validate():
@@ -74,11 +90,12 @@ def register():
                     (name, username, email, password))
         # commit
         mysql.connection.commit()
-        flask.flash("You have successfully registered in our wonderful App", 'sucess')
+        flask.flash("You have successfully registered in our wonderful App", 'success')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
-#login
+
+# login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -88,44 +105,59 @@ def login():
         result = cur.execute("select * from users where username = %s", [username])
         if result > 0:
             data = cur.fetchone()
-            if sha256_crypt.verify(password_C,data['password']):
-                #redirect
+            if sha256_crypt.verify(password_C, data['password']):
+                # redirect
                 session['logged_in'] = True
                 session['username'] = username
-                flash("You are now logged in",'sucess')
-                return  redirect(url_for('dashboard'))
+                flash("You are now logged in", 'success')
+                return redirect(url_for('dashboard'))
             else:
-                error='NOT MATCHED'
-                return render_template('login.html',error=error)
+                error = 'NOT MATCHED'
+                return render_template('login.html', error=error)
             cur.close()
         else:
             error = 'User dosen\'t exist'
-            return render_template('login.html',error=error)
+            return render_template('login.html', error=error)
     return render_template('login.html')
-#checks if logged_in
+
+
+# checks if logged_in
 def isLoggedin(f):
     @wraps(f)
     def wrap(*args, **kwargs):
         if 'logged_in' in session:
-            return f(*args,**kwargs)
+            return f(*args, **kwargs)
         else:
-            flash("Unautherized, please login",'danger')
+            flash("Unautherized, please login", 'danger')
             return redirect(url_for('login'))
+
     return wrap
-#dashboard
+
+
+# dashboard
 @app.route('/dashboard')
 @isLoggedin
 def dashboard():
+    cur = mysql.connection.cursor()
+    result = cur.execute("select * from articles")
+
+    if result > 0:
+        articles = cur.fetchall()
+        cur.close()
+        return render_template('/dashboard.html', articles=articles)
     return render_template('/dashboard.html')
-#logout
+
+
+# logout
 @app.route('/logout')
 @isLoggedin
 def logout():
     session.clear()
-    flash("You have logged out sucessfully",'sucess')
+    flash("You have logged out sucessfully", 'success')
     return redirect(url_for('login'))
 
-@app.route('/addarticle',methods=['GET','POST'])
+
+@app.route('/addarticle', methods=['GET', 'POST'])
 @isLoggedin
 def addArticle():
     form = AddArticleForm(request.form)
@@ -134,12 +166,13 @@ def addArticle():
         title = form.title.data
         body = form.body.data
         auther = session['username']
-        cur.execute("INSERT into articles(title,body,auther) value(%s,%s,%s)",(title,body,auther))
+        cur.execute("INSERT into articles(title,body,auther) value(%s,%s,%s)", (title, body, auther))
         mysql.connection.commit()
         cur.close()
-        flash("Article add successfulyy",'sucess')
+        flash("Article add successfulyy", 'success')
         return redirect(url_for('dashboard'))
-    return render_template('add_article.html',form=form)
+    return render_template('add_article.html', form=form)
+
 
 if __name__ == '__main__':
     app.run()
