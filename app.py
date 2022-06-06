cd41749a175acca705a469593b947595c7e18b7a
hoss1shark <64555397+hoss1shark@users.noreply.github.com>
2022-06-06 14:14:43 +0200

    login and access control

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import flask
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,flash,session,redirect,url_for
 from data import Articles
-from wtforms import Form, TextAreaField, BooleanField, StringField, PasswordField, validators
+from wtforms import Form, StringField, PasswordField, validators
 from flask_mysqldb import MySQL
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 app = Flask(__name__)
 Articles = Articles()
-#Configuring Mysql
+# Configuring Mysql
 app.config.update(
     TESTING=True,
     SECRET_KEY='192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf'
@@ -18,8 +19,10 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'myflaskapp'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-#inti MySQL
-mysql =MySQL(app)
+# inti MySQL
+mysql = MySQL(app)
+
+
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('/Home.html')
@@ -60,13 +63,62 @@ def register():
         username = form.username.data
         email = form.email.data
         password = sha256_crypt.encrypt(str(form.password.data))
-        #create dbcursor
+        # create dbcursor
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(name,username,email,password) values (%s,%s,%s,%s)",(name,username,email,password))
-        #commit
+        cur.execute("INSERT INTO users(name,username,email,password) values (%s,%s,%s,%s)",
+                    (name, username, email, password))
+        # commit
         mysql.connection.commit()
-        flask.flash("You have successfully registered in our wonderful App",'sucess')
+        flask.flash("You have successfully registered in our wonderful App", 'sucess')
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
+
+#login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password_C = request.form['password']
+        cur = mysql.connection.cursor()
+        result = cur.execute("select * from users where username = %s", [username])
+        if result > 0:
+            data = cur.fetchone()
+            if sha256_crypt.verify(password_C,data['password']):
+                #redirect
+                session['logged_in'] = True
+                session['username'] = username
+                flash("You are now logged in",'sucess')
+                return  redirect(url_for('dashboard'))
+            else:
+                error='NOT MATCHED'
+                return render_template('login.html',error=error)
+            cur.close()
+        else:
+            error = 'User dosen\'t exist'
+            return render_template('login.html',error=error)
+    return render_template('login.html')
+#checks if logged_in
+def isLoggedin(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args,**kwargs)
+        else:
+            flash("Unautherized, please login",'danger')
+            return redirect(url_for('login'))
+    return wrap
+#dashboard
+@app.route('/dashboard')
+@isLoggedin
+def dashboard():
+    return render_template('/dashboard.html')
+#logout
+@app.route('/logout')
+@isLoggedin
+def logout():
+    session.clear()
+    flash("You have logged out sucessfully",'sucess')
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
